7c62aa322d606bd9cbcfbef8a74dc4ba6ac05172
unknown <bika070705@gmail.com>
2024-02-25 09:13:59 +0200

    choto

--- js/index.js
+++ js/index.js
@@ -5,3 +5,34 @@ const obj1 = {
 const obj2 = {
   name: 'Object 2',
 };
+
+function logName() {
+  console.log(this.name);
+}
+
+obj1.logName = logName;
+obj2.logName = logName;
+
+
+obj1.logName();
+obj2.logName();
+
+const boundFunction = logName.bind(obj1);
+boundFunction(); 
+
+const arrowLogName = () => {
+  console.log(this.name);
+};
+
+
+const objWithGreeting = {
+  greeting: 'Привет',
+  names: ['Жаныбек', 'Алина', 'Иван'],
+  greetEveryone: function () {
+    this.names.forEach((name) => {
+      console.log(`${this.greeting}, ${name}!`);
+    });
+  },
+};
+
+objWithGreeting.greetEveryone();
